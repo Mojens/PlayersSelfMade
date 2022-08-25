63167f71b4836be85db70b4f748e5b0628ecbe8a
Mojens <moha135h@stud.kea.dk>
2022-08-25 15:28:54 +0200

    fejl

--- src/test/java/com/example/forstedagtrejdesemesterpt2/repositories/PlayerRepositoryTest.java
+++ src/test/java/com/example/forstedagtrejdesemesterpt2/repositories/PlayerRepositoryTest.java
@@ -1,4 +1,15 @@
+package com.example.forstedagtrejdesemesterpt2.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlayerRepositoryTest {
-  
+
+  @Test
+  void fail(){
+    assertEquals(1,0);
+  }
+
 }
